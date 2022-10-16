2c7682597fe4d449f670143f91f2605b682234c4
Jacob <jacobbward0@gmail.com>
2022-10-16 10:10:55 +0100

    Rename presentation modules.

--- src/presentation/holder.rs
+++ src/presentation/holder.rs
@@ -1,14 +1,3 @@
-//! Mdoc authentication.
-//!
-//! 1. As part of mdoc response, mdl produces `DeviceAuth`, which is either a `DeviceSignature` or
-//!    a `DeviceMac`.
-//!
-//! 2. The reader must authenticate that `DeviceKey` in the MSO is the key that generated the
-//!    `DeviceAuth`.
-//!
-//! 3. The reader must authenticate that the `DeviceKey` is authorized by `KeyAuthorizations` to
-//!    sign over the data elements present in `DeviceNameSpaces`.
-
 use crate::definitions::{
     device_request::DeviceRequest,
     device_response::{
