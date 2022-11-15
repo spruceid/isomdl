061fce550bd352e891e9994bfa38a659736cf7cf
Jacob <jacobbward0@gmail.com>
2022-11-15 15:46:40 +0000

    Add FullDate str roundtrip test

--- src/definitions/fulldate.rs
+++ src/definitions/fulldate.rs
@@ -1,6 +1,6 @@
 use anyhow::Error;
 use serde_cbor::Value;
-use std::str::FromStr;
+use std::{fmt, str::FromStr};
 use time::{format_description::FormatItem, macros::format_description, Date};
 
 const FORMAT: &[FormatItem<'static>] = format_description!("[year]-[month]-[day]");
@@ -10,14 +10,18 @@ pub struct FullDate(Date);
 
 impl From<FullDate> for Value {
     fn from(d: FullDate) -> Value {
-        Value::Tag(
-            1004,
-            Box::new(Value::Text(format!(
-                "{}-{}-{}",
-                d.0.year(),
-                <u8>::from(d.0.month()),
-                d.0.day()
-            ))),
+        Value::Tag(1004, Box::new(Value::Text(d.to_string())))
+    }
+}
+
+impl fmt::Display for FullDate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}-{}-{}",
+            self.0.year(),
+            <u8>::from(self.0.month()),
+            self.0.day()
         )
     }
 }
@@ -27,5 +31,17 @@ impl FromStr for FullDate {
 
     fn from_str(s: &str) -> Result<Self, Error> {
         Ok(FullDate(Date::parse(s, FORMAT)?))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn fulldate_str_roundtrip() {
+        const DATESTR: &str = "2000-12-30";
+        let fulldate = FullDate::from_str(DATESTR).expect("unable to parse datestr");
+        assert_eq!(DATESTR, fulldate.to_string())
     }
 }
