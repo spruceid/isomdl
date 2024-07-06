f8b393c0c369ba5ab8dfd4f63d687bce3fa9bb4c
Radu Marias <radumarias@gmail.com>
2024-07-07 00:26:14 +0300

    organize example and added more docs

--- examples/README.md
+++ examples/README.md
@@ -6,15 +6,26 @@
  cargo run --package isomdl_examples --bin device_and_reader
 ```
 
-This exemplifies a connection between a device and a reader.
-The reader asks for `age_over_21` element and device responds with that value.
-Flow is like this:
-- device initializes with the mDL data, private and public keys
-- device creates a QR code with `DeviceEngagement` data which contains it's public key also
-- reader processes the QR code and creates a request for `age_over_21` element
-  - also generating it's private an public keys and initiating key exchange and generates the session keys
-  - the request is encrypted with the reader session key
-- device receives the request and also initiates key exchange and generates the session keys
-- device decrypts the request with reader's session key and create the response with `age_over_21` element encrypted with the device session key
-- reader process the response and prints the value of `age_over_21` element
+This example demonstrates a connection between a device and a reader.  
+The reader requests the age_over_21 element, and the device responds with that value.
+The flow is as follows:
 
+1. **Device initialization and engagement:**
+    - The device creates a QR code containing `DeviceEngagement` data, which includes its public key.
+    - Internally:
+        - The device initializes with the mDL data, private key, and public key.
+2. **Reader processing QR and requesting needed fields:**
+    - The reader processes the QR code and creates a request for the `age_over_21` element.
+    - Internally:
+        - Generates its private and public keys.
+        - Initiates a key exchange, and generates the session keys.
+        - The request is encrypted with the reader's session key.
+3. **Device accepting request and responding:**
+    - The device receives the request and creates a response with the `age_over_21` element.
+    - Internally:
+        - Initiates the key exchange, and generates the session keys.
+        - Decrypts the request with the reader's session key.
+        - Parse and validate it creating error response if needed.
+        - The response is encrypted with the device's session key.
+4. **Reader Processing mDL data:**
+    - The reader processes the response and prints the value of the `age_over_21` element.
--- examples/src/device_and_reader.rs
+++ examples/src/device_and_reader.rs
@@ -1,79 +1,127 @@
 use anyhow::{Context, Result};
 use std::collections::BTreeMap;
 
-use isomdl::definitions::device_request::{DataElements, Namespaces};
-use isomdl::presentation::device::{Document, Documents, PermittedItems};
+use isomdl::definitions::device_request::{DataElements, DocType, Namespaces};
+use isomdl::definitions::helpers::NonEmptyMap;
+use isomdl::presentation::device::{Document, Documents, PermittedItems, RequestedItems};
 use isomdl::presentation::{device, reader, Stringify};
 
+const DOC_TYPE: &str = "org.iso.18013.5.1.mDL";
+const NAMESPACE: &str = "org.iso.18013.5.1";
+const AGE_OVER_21_ELEMENT: &str = "age_over_21";
+
 fn main() -> Result<()> {
-    let mdl_encoded = include_str!("../../test/stringified-mdl.txt").to_string();
+    let mdl_encoded = include_str!("../../test/stringified-mdl.txt");
 
-    let mdl = Document::parse(mdl_encoded).context("could not parse mdl")?;
-    let docs = Documents::new("org.iso.18013.5.1.mDL".to_string(), mdl);
+    // Parse the mDL
+    let docs = parse_mdl(mdl_encoded)?;
 
-    // init device
-    let (device_sm_engaged, qr) = device::SessionManagerInit::initialise(docs, None, None)?
-        // create qr code
-        .qr_engagement()?;
+    // Device initialization and engagement
+    let (device_sm_engaged, qr) = initialize_and_engage_device(docs)?;
 
-    // define required elements by the reader
-    let requested_elements = Namespaces::new(
-        "org.iso.18013.5.1".into(),
-        DataElements::new("age_over_21".to_string(), false),
-    );
+    // Reader processing QR and requesting needed fields
+    let (mut reader_sm, session_request) = establish_reader_session(qr)?;
 
-    // reader scan qr code
-    let (mut reader_sm, session_request, _) =
-        reader::SessionManager::establish_session(qr, requested_elements)?;
-
-    // device accept reader request
-    let (mut device_sm, items) = device_sm_engaged.process_session_establishment(
-        serde_cbor::value::from_value(serde_cbor::from_slice(&session_request)?)?,
-    )?;
-
-    // check for any errors
-    while let Some((_, _to_sign)) = device_sm.get_next_signature_payload() {
-        // todo: implement signing
-        device_sm.submit_next_signature(vec![1, 2, 3, 4, 5])?;
-    }
-    if device_sm.response_ready() {
-        // we got an error, send it to reader
-        let res = reader_sm.handle_response(
-            &device_sm
-                .retrieve_response()
-                .ok_or_else(|| anyhow::anyhow!("no response to retrieve"))?,
-        )?;
-        println!("reader response: {res:?}");
-        println!("we sent errors, terminating");
+    // Device accepting request and validating
+    let (mut device_sm, requested_items) =
+        device_accept_request(device_sm_engaged, session_request)?;
+    // Propagate any errors
+    if let Ok(Some(response)) = check_for_errors(&mut device_sm) {
+        let res = reader_sm.handle_response(&response)?;
+        println!("Reader: {res:?}");
+        println!("Errors sent, terminating.");
         return Ok(());
-    }
+    };
 
-    // device prepare response
+    // Prepare response with required elements
+    let response = prepare_device_response(&mut device_sm, requested_items)?;
+
+    // Reader Processing mDL data
+    reader_handle_device_response(&mut reader_sm, response)?;
+
+    Ok(())
+}
+
+/// Check if there were any errors and sign them if needed, returning the response error.
+fn check_for_errors(device_sm: &mut device::SessionManager) -> Result<Option<Vec<u8>>> {
+    while let Some(_to_sign) = device_sm.get_next_signature_payload() {
+        // TODO: Implement actual signing mechanism
+        device_sm
+            .submit_next_signature(vec![1, 2, 3, 4, 5])
+            .context("failed to submit signature")?;
+    }
+    Ok(device_sm.retrieve_response())
+}
+
+/// Parse the mDL encoded string into a [`Documents`] object.
+fn parse_mdl(encoded: &str) -> Result<NonEmptyMap<DocType, Document>> {
+    let mdl = Document::parse(encoded.to_string()).context("could not parse mDL")?;
+    let docs = Documents::new(DOC_TYPE.to_string(), mdl);
+    Ok(docs)
+}
+
+/// Creates a QR code containing `DeviceEngagement` data, which includes its public key.
+fn initialize_and_engage_device(
+    docs: Documents,
+) -> Result<(device::SessionManagerEngaged, String)> {
+    device::SessionManagerInit::initialise(docs, None, None)
+        .context("failed to initialize device")?
+        .qr_engagement()
+}
+
+/// Establishes the reader session from the given QR code and create request for needed elements.
+fn establish_reader_session(qr: String) -> Result<(reader::SessionManager, Vec<u8>)> {
+    let requested_elements = Namespaces::new(
+        NAMESPACE.into(),
+        DataElements::new(AGE_OVER_21_ELEMENT.to_string(), false),
+    );
+    let (reader_sm, session_request, _) =
+        reader::SessionManager::establish_session(qr, requested_elements)
+            .context("failed to establish reader session")?;
+    Ok((reader_sm, session_request))
+}
+
+/// The Device accepts request and validates it returning requested items.
+fn device_accept_request(
+    device_sm_engaged: device::SessionManagerEngaged,
+    session_request: Vec<u8>,
+) -> Result<(device::SessionManager, RequestedItems)> {
+    let (device_sm, items) = device_sm_engaged.process_session_establishment(
+        serde_cbor::value::from_value(serde_cbor::from_slice(&session_request)?)
+            .context("Failed to process session establishment")?,
+    )?;
+    Ok((device_sm, items))
+}
+
+// Prepare response with required elements.
+fn prepare_device_response(
+    device_sm: &mut device::SessionManager,
+    items: RequestedItems,
+) -> Result<Vec<u8>> {
     let mut permitted = PermittedItems::new();
     let mut fields = BTreeMap::new();
-    fields.insert(
-        "org.iso.18013.5.1".to_string(),
-        vec!["age_over_21".to_string()],
-    );
-    permitted.insert("org.iso.18013.5.1.mDL".into(), fields);
+    fields.insert(NAMESPACE.to_string(), vec![AGE_OVER_21_ELEMENT.to_string()]);
+    permitted.insert(DOC_TYPE.into(), fields);
+
     device_sm.prepare_response(&items, permitted);
     while let Some((_, _to_sign)) = device_sm.get_next_signature_payload() {
-        // todo: implement signing
-        device_sm.submit_next_signature(vec![1, 2, 3, 4, 5])?;
+        // TODO: Implement actual signing mechanism
+        device_sm
+            .submit_next_signature(vec![1, 2, 3, 4, 5])
+            .context("failed to submit signature")?;
     }
+    Ok(device_sm
+        .retrieve_response()
+        .ok_or(anyhow::anyhow!("failed to prepare response"))
+        .unwrap())
+}
 
-    // send response to reader
-    if device_sm.response_ready() {
-        // send response to reader
-        let res = reader_sm.handle_response(
-            &device_sm
-                .retrieve_response()
-                .ok_or_else(|| anyhow::anyhow!("no response to retrieve"))?,
-        )?;
-        println!("{:?}", res);
-    } else {
-        // todo: is this a valid state?
-    }
-
+/// Reader Processing mDL data.
+fn reader_handle_device_response(
+    reader_sm: &mut reader::SessionManager,
+    response: Vec<u8>,
+) -> Result<()> {
+    let res = reader_sm.handle_response(&response)?;
+    println!("{:?}", res);
     Ok(())
 }
