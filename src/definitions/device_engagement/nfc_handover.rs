2640ae1ee0120e9991f798925fad5e2be488f9d0
Ryan Tate <ryan.tate@spruceid.com>
2025-08-19 16:58:51 -0700

    wip: construct urn handover select ndef message
    Signed-off-by: Ryan Tate <ryan.tate@spruceid.com>

--- src/definitions/device_engagement/nfc_handover.rs
+++ src/definitions/device_engagement/nfc_handover.rs
@@ -1,11 +1,18 @@
 use std::borrow::Cow;
 
-use ndef_rs::{self as ndef, payload::{RecordPayload, UriPayload}, NdefMessage, NdefRecord};
+use ndef_rs::{
+    self as ndef,
+    payload::{RecordPayload, UriPayload},
+    NdefMessage, NdefRecord,
+};
 use serde::{Deserialize, Serialize};
 
 use super::{DeviceEngagement, DeviceRetrievalMethod};
-use crate::definitions::helpers::{ByteStr, Tag24};
 use crate::definitions::session::Error;
+use crate::definitions::{
+    device_engagement::CentralClientMode,
+    helpers::{ByteStr, Tag24},
+};
 
 /// When negotiated handover is used, the mdoc (holder) should include
 /// the following service URN to the reader, which will be used to select
@@ -43,33 +50,38 @@ impl<'r, 'p> RecordPayload for RawPayload<'r, 'p> {
 pub struct NfcHandover(pub NfcHandoverSelectMessage, pub NfcHandoverRequestMessage);
 
 impl NfcHandover {
-
     pub fn create_direct_handover() -> Result<Self, Error> {
-        let tp_payload = [&[
-            0x10, // TNEP version 1.0
-            NFC_NEGOTIATED_HANDOVER_SERVICE.len() as u8, // Length of service URN
-        ], NFC_NEGOTIATED_HANDOVER_SERVICE.as_bytes(), &[
-            0x00, // Single response communication mode
-            0x10, // Minimum wait time. TODO: magic number
-            0x0F, // Maximum no. of time extensions. TODO: magic number
-            0xFF, // Max NDEF size (upper bits)
-            0xFF, // Max NDEF size (lower bits)
-        ]].concat();
+        let tp_payload = [
+            &[
+                0x10,                                        // TNEP version 1.0
+                NFC_NEGOTIATED_HANDOVER_SERVICE.len() as u8, // Length of service URN
+            ],
+            NFC_NEGOTIATED_HANDOVER_SERVICE.as_bytes(),
+            &[
+                0x00, // Single response communication mode
+                0x10, // Minimum wait time. TODO: magic number
+                0x0F, // Maximum no. of time extensions. TODO: magic number
+                0xFF, // Max NDEF size (upper bits)
+                0xFF, // Max NDEF size (lower bits)
+            ],
+        ]
+        .concat();
 
         // Create the Tp (Transport Protocol) record
-        let tp_record = NdefMessage::from(&[
-            NdefRecord::builder()
-                .tnf(ndef::TNF::WellKnown)
-                .payload(&RawPayload {
-                    record_type: b"Tp",
-                    payload: &tp_payload,
-                })
-                .build()?,
-        ]);
+        let tp_record = NdefMessage::from(&[NdefRecord::builder()
+            .tnf(ndef::TNF::WellKnown)
+            .payload(&RawPayload {
+                record_type: b"Tp",
+                payload: &tp_payload,
+            })
+            .build()?]);
 
         // Final top-level NDEF message
         Ok(NfcHandover(
-            tp_record.to_buffer().map_err(Error::NdefSerialization)?.into(),
+            tp_record
+                .to_buffer()
+                .map_err(Error::NdefSerialization)?
+                .into(),
             None,
         ))
     }
@@ -78,12 +90,12 @@ impl NfcHandover {
         device_engagement: &Tag24<DeviceEngagement>,
         nfc_handover_request: NfcHandoverRequestMessage,
     ) -> Result<Self, Error> {
-
-        todo!();
-
         let uri_record = NdefRecord::builder()
             .tnf(ndef::TNF::WellKnown)
-            .payload(&UriPayload::static_with_abbrev(ndef::NONE_ABBRE, NFC_NEGOTIATED_HANDOVER_SERVICE))
+            .payload(&UriPayload::static_with_abbrev(
+                ndef::NONE_ABBRE,
+                NFC_NEGOTIATED_HANDOVER_SERVICE,
+            ))
             .build()?;
 
         let mut message = NdefMessage::from(&[uri_record]);
@@ -91,39 +103,47 @@ impl NfcHandover {
         // Dynamic AC/Carrier records
         if let Some(retrieval_methods) = device_engagement.inner.device_retrieval_methods.as_ref() {
             for method in retrieval_methods.iter() {
-                if let DeviceRetrievalMethod::BLE(_) = method {
-                    // TODO: Do we want to be completely ignoring the reported method?
+                if let DeviceRetrievalMethod::BLE(options) = method {
+                    if let Some(CentralClientMode { uuid }) = options.central_client_mode.as_ref() {
+                        // TODO: Do we want to be completely ignoring the reported method?
+                        const CARRIER_DATA_REFERENCE_ID: u8 = b'B';
 
-                    
+                        let ac_record = NdefRecord::builder()
+                            .tnf(ndef::TNF::WellKnown)
+                            .payload(&RawPayload {
+                                record_type: b"ac",
+                                payload: &[
+                                    0x01, // Carrier Power State: active
+                                    0x01, // Length of Carrier Data Reference
+                                    CARRIER_DATA_REFERENCE_ID,
+                                    0x00, // Auxiliary Data Reference Count
+                                ],
+                            })
+                            .build()?;
 
-                    /*
-                    const CARRIER_DATA_REFERENCE_ID: u8 = b'B';
+                        let bt_record = NdefRecord::builder()
+                            .tnf(ndef::TNF::MimeMedia)
+                            .payload(&RawPayload {
+                                record_type: b"application/vnd.bluetooth.ep.oob",
+                                payload: &[
+                                    [
+                                        0x02, // Length of LE Role Payload
+                                        0x1C, // LE Role Advertising Data Type
+                                        0x01, // Central Client Mode BLE role
+                                        // Following represents the service UUIDs
+                                        0x11, 0x07,
+                                    ]
+                                    .as_slice(),
+                                    uuid.as_bytes(),
+                                ]
+                                .concat(),
+                            })
+                            .id(vec![CARRIER_DATA_REFERENCE_ID]) // must match AC reference
+                            .build()?;
 
-                    let ac_record = NdefRecord::builder()
-                        .tnf(ndef::TNF::WellKnown)
-                        .payload(&RawPayload{
-                            record_type: b"ac",
-                            payload: &[
-                                0x01, // Carrier Power State: active
-                                0x01, // Length of Carrier Data Reference
-                                CARRIER_DATA_REFERENCE_ID,
-                                0x00, // Auxiliary Data Reference Count
-                            ],
-                        })
-                        .build()?;
-
-                    let bt_record = NdefRecord::builder()
-                        .tnf(ndef::TNF::MimeMedia)
-                        .payload(&RawPayload {
-                            record_type: b"application/vnd.bluetooth.ep.oob",
-                            payload: &device_engagement.inner_bytes,
-                        })
-                        .id(vec![CARRIER_DATA_REFERENCE_ID]) // must match AC reference
-                        .build()?;
-
-                    message.add_record(ac_record);
-                    message.add_record(bt_record);
-                    */
+                        message.add_record(ac_record);
+                        message.add_record(bt_record);
+                    }
                 }
             }
         }
@@ -131,19 +151,20 @@ impl NfcHandover {
         let mut hs_payload = vec![0x12]; // Version 1.2
         hs_payload.extend_from_slice(&message.to_buffer().map_err(Error::NdefSerialization)?);
 
-        let hs_record = NdefMessage::from(&[
-            NdefRecord::builder()
-                .tnf(ndef::TNF::WellKnown)
-                .payload(&RawPayload {
-                    record_type: b"Hs",
-                    payload: &hs_payload,
-                })
-                .build()?,
-        ]);
+        let hs_record = NdefMessage::from(&[NdefRecord::builder()
+            .tnf(ndef::TNF::WellKnown)
+            .payload(&RawPayload {
+                record_type: b"Hs",
+                payload: &hs_payload,
+            })
+            .build()?]);
 
         // Final top-level NDEF message
         Ok(NfcHandover(
-            hs_record.to_buffer().map_err(Error::NdefSerialization)?.into(),
+            hs_record
+                .to_buffer()
+                .map_err(Error::NdefSerialization)?
+                .into(),
             nfc_handover_request,
         ))
     }
