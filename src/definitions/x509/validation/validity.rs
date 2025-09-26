121c4bde9cbbdf1ee6d6896ebf76480c7896f801
Ryan Tate <ryan.tate@spruceid.com>
2025-09-26 16:09:32 -0700

    add wasm-bindgen feature flag to time crate
    Signed-off-by: Ryan Tate <ryan.tate@spruceid.com>

--- src/definitions/x509/validation/validity.rs
+++ src/definitions/x509/validation/validity.rs
@@ -2,16 +2,13 @@ use time::OffsetDateTime;
 use x509_cert::Certificate;
 
 pub fn check_validity_period(certificate: &Certificate) -> Vec<Error> {
+    let current_time = OffsetDateTime::now_utc();
     let validity = certificate.tbs_certificate.validity;
     let mut errors: Vec<Error> = vec![];
-    if validity.not_after.to_unix_duration().as_secs()
-        < OffsetDateTime::now_utc().unix_timestamp() as u64
-    {
+    if validity.not_after.to_unix_duration().as_secs() < current_time.unix_timestamp() as u64 {
         errors.push(Error::Expired);
     };
-    if validity.not_before.to_unix_duration().as_secs()
-        > OffsetDateTime::now_utc().unix_timestamp() as u64
-    {
+    if validity.not_before.to_unix_duration().as_secs() > current_time.unix_timestamp() as u64 {
         errors.push(Error::NotYetValid);
     };
 
