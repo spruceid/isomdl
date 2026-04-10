08818f746e79822f2aed23c1e25e9fab7b63722c
Allison Pierson <allison.pierson@spruceid.com>
2026-04-10 16:21:05 -0500

    add HMAC0 test

--- tests/simulated_device_and_reader.rs
+++ tests/simulated_device_and_reader.rs
@@ -35,3 +35,31 @@ pub async fn simulated_device_and_reader_interaction() {
         .await
         .unwrap();
 }
+
+#[test_log::test(tokio::test)]
+pub async fn simulated_device_and_reader_interaction_mac0() {
+    // Device initialization and engagement
+    let engaged_state = Device::initialise_session().unwrap();
+
+    // Reader processing QR and requesting the necessary fields
+    let (mut reader_session_manager, request) =
+        Device::establish_reader_session(engaged_state.qr_handover().unwrap()).unwrap();
+
+    // Device accepting request
+    let (device_session_manager, validated_request) =
+        Device::handle_request(engaged_state, request, Default::default())
+            .await
+            .unwrap();
+
+    // Prepare response with required elements using COSE_Mac0
+    let response = Device::create_response_mac0(
+        device_session_manager,
+        &validated_request.items_request,
+    )
+    .unwrap();
+
+    // Reader Processing mDL data
+    Reader::reader_handle_device_response(&mut reader_session_manager, response)
+        .await
+        .unwrap();
+}
